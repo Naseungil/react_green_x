9e92c33a794a7f67108c4da19ed54ca226bb635b
Naseungil <sktmddlf505@naver.com>
2024-11-08 15:09:49 +0900

    로그인 완료

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { authService } from "../firebase";
 import AppRouter from "./Router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 console.log(authService);
 
 function App() {
   const [isLoggedIn, setIsLoggidIn] = useState(false);
-  return <AppRouter isLoggedIn={isLoggedIn} />;
+  const [init, setInit] = useState(false); //로그인 여부 확인 시작 여부
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        //유저정보가 있다면
+        setIsLoggidIn(true);
+      } else {
+        setIsLoggidIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return (
+    <>{init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing...."}</>
+  );
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+
+  const auth = getAuth();
+
+  const onChange = (e) => {
+    /*
+     setEmail(e.target.email);
+     setPassword(e.target.password); <- 이렇게 쓰면 안됨
+
+    if (e.target.name === "email") {
+      setEmail(e.target.email);
+    } else if (e.target.name === "password") {
+      setPassword(e.target.password);
+    }
+    */
+
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (newAccount) {
+      //회원가입
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          //계정생성완료후, 로그인 완료
+          const user = userCredential.user; //생성된 계정의 유저정보 확인
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setError(errorMessage);
+        });
+    } else {
+      //로그인
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setError(errorMessage);
+        });
+    }
+  };
+  const toggleAccount = () => {
+    setNewAccount((prev) => !prev);
+  };
+  return (
+    <div className="container">
+      <h1>{newAccount ? "회원가입" : "로그인"}</h1>
+      <Form onSubmit={onSubmit}>
+        <Form.Group className="mb-3" controlId="loginEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            name="email"
+            type="email"
+            onChange={onChange}
+            placeholder="name@example.com"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="loginPW">
+          <Form.Label>Password</Form.Label>
+          <Form.Control name="password" type="password" onChange={onChange} />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          {newAccount ? "회원가입" : "로그인"}
+        </Button>
+        <div>{error}</div>
+      </Form>
+      <hr />
+      <Button variant="secondary" onClick={toggleAccount}>
+        {newAccount ? "로그인으로 전환" : "회원가입으로 전환"}
+      </Button>
+    </div>
+  );
 };
 
 export default Auth;
