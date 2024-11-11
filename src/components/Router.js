7bebed911131ac86de835fb84547447b4c25b323
Naseungil <sktmddlf505@naver.com>
2024-11-11 12:02:41 +0900

    유저정보 전달

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,7 +5,7 @@ import Home from "../routes/Home";
 import Nav from "./Nav";
 import Profile from "../routes/Profile";
 
-const AppRouter = ({ isLoggedIn }) => {
+const AppRouter = ({ isLoggedIn, userObj }) => {
   return (
     <>
       {isLoggedIn && <Nav />}
@@ -13,7 +13,7 @@ const AppRouter = ({ isLoggedIn }) => {
         {
           isLoggedIn ? (
             <>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Home userObj={userObj} />} />
               <Route path="/profile" element={<Profile />} />
             </> //참
           ) : (
