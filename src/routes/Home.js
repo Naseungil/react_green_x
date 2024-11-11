905d5c13554f8bc8039f9b2e79782b04780c8968
Naseungil <sktmddlf505@naver.com>
2024-11-11 09:40:53 +0900

    폼 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import { Button } from "react-bootstrap";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="글을 입력해주세요" />
+        </Form.Group>
+        <Button variant="primary">입력</Button>
+      </Form>
+    </div>
+  );
 };
 
 export default Home;
