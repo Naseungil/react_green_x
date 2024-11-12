ec5c062f9741a82634b9e4d4f290102dffc01037
Naseungil <sktmddlf505@naver.com>
2024-11-12 09:57:08 +0900

    프로필 이미지 변경

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -2,6 +2,7 @@ import React from "react";
 import { getAuth, signOut } from "firebase/auth";
 import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router-dom";
+import Form from "react-bootstrap/Form";
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -21,6 +22,23 @@ const Profile = () => {
   return (
     <div>
       <h1>Profile Page</h1>
+      <div className="profile">
+        <img src={`${process.env.PUBLIC_URL}/profile_icon.svg`} alt="" />
+      </div>
+
+      <input
+        type="text"
+        className="hidden"
+        accept="image/*"
+        name="profile"
+        id="profile"
+      />
+
+      <Button type="submit" variant="primary">
+        프로필 이미지 변경
+      </Button>
+
+      <hr />
       <Button variant="primary" onClick={onLogoutClick}>
         로그아웃
       </Button>
