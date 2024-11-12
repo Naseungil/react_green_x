161d0fdc2bb74f38bb19f98da99d5e89620b382b
Naseungil <sktmddlf505@naver.com>
2024-11-12 11:14:10 +0900

    프로필 이미지

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,10 +1,16 @@
-import React from "react";
-import { getAuth, signOut } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { getAuth, signOut, updateProfile } from "firebase/auth";
 import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router-dom";
-import Form from "react-bootstrap/Form";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { authService } from "../firebase";
+console.log(authService);
 
 const Profile = () => {
+  const user = authService.currentUser;
+  const profilePath = `${process.env.PUBLIC_URL}/profile_icon.svg`;
+  const [profile, setProfile] = useState(profilePath);
+
   const navigate = useNavigate();
   const onLogoutClick = () => {
     const auth = getAuth();
@@ -19,11 +25,33 @@ const Profile = () => {
         alert(error);
       });
   };
+
+  const updateLogo = async (e) => {
+    const file = e.target.files[0];
+    const storage = getStorage(); //파일업로드 관련 함수 초기화
+    const storageRef = ref(storage, `profile/${user.uid}`); //파일이 저장될 위치, 이름지정
+
+    const result = await uploadBytes(storageRef, file); //업로드후 결과를 result에 할당
+
+    const profileURL = await getDownloadURL(result.ref); //업로드된 이미지의 절대 경로
+
+    setProfile(profileURL); //새이미지로 프로필 교체
+
+    const auth = getAuth();
+    await updateProfile(auth.currentUser, {
+      photoURL: profileURL,
+    });
+  };
+
+  useEffect(() => {
+    user.photoURL !== null && setProfile(authService.currentUser.photoURL);
+  }, []); //최초한번 실행, 값이 변경되면 실행
+
   return (
-    <div>
+    <div className="container">
       <h1>Profile Page</h1>
       <div className="profile">
-        <img src={`${process.env.PUBLIC_URL}/profile_icon.svg`} alt="" />
+        <img src={profile} alt="" />
       </div>
 
       <input
@@ -32,11 +60,12 @@ const Profile = () => {
         accept="image/*"
         name="profile"
         id="profile"
+        onChange={updateLogo}
       />
 
-      <Button type="submit" variant="primary">
+      <label htmlFor="profile" className="btn btn-primary">
         프로필 이미지 변경
-      </Button>
+      </label>
 
       <hr />
       <Button variant="primary" onClick={onLogoutClick}>
